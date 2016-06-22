bfa4e43cd8c4e45568be7f521633168699f8b5f6
ou3265824 <632521056@qq.com>
2016-06-22 23:19:57 +0800

    添加

---
+++ workframe/src/main/java/com/example/my/http/backcall/RBackCall.java
@@ -0,0 +1,16 @@
+package com.example.my.http.backcall;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Administrator on 2016/4/27.
+ */
+public interface RBackCall<T> extends Callback<T> {
+
+
+
+
+
+}
